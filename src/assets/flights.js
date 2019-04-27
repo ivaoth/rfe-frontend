12877534c99e56713e8749065bcab6dafb4e0eae
Phumrapee Limpianchop <git@rayriffy.com>
2019-04-28 02:01:51 +0700

    :memo: Mockuop database

---
+++ src/assets/flights.js
@@ -0,0 +1,1234 @@
+export const mockupFlight = [
+  {
+    flight: 'FD3221',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSG',
+    },
+    aircraft: '320',
+    time: {
+      departure: '10:00',
+    },
+  },
+  {
+    flight: 'SL962',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZBTJ',
+    },
+    aircraft: 'B739',
+    time: {
+      departure: '10:00',
+    },
+  },
+  {
+    flight: 'TR869',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'WSSS',
+    },
+    aircraft: '789',
+    time: {
+      departure: '10:00',
+    },
+  },
+  {
+    flight: 'DD7618',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSE',
+    },
+    aircraft: 'DH4',
+    time: {
+      departure: '10:05',
+    },
+  },
+  {
+    flight: 'FD177',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VRMM',
+    },
+    aircraft: '320',
+    time: {
+      departure: '10:05',
+    },
+  },
+  {
+    flight: 'FD568',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZSHC',
+    },
+    aircraft: '320',
+    time: {
+      departure: '10:05',
+    },
+  },
+  {
+    flight: 'SL762',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSP',
+    },
+    aircraft: 'B739',
+    time: {
+      departure: '10:05',
+    },
+  },
+  {
+    flight: 'FD3306',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTPP',
+    },
+    aircraft: '320',
+    time: {
+      departure: '10:10',
+    },
+  },
+  {
+    flight: 'QZ257',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'WIII',
+    },
+    aircraft: '320',
+    time: {
+      departure: '10:10',
+    },
+  },
+  {
+    flight: 'DD9318',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUU',
+    },
+    aircraft: '738',
+    time: {
+      departure: '10:15',
+    },
+  },
+  {
+    flight: 'SL924',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZSNB',
+    },
+    aircraft: 'B739',
+    time: {
+      departure: '10:15',
+    },
+  },
+  {
+    flight: 'DD7114',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSS',
+    },
+    aircraft: '738',
+    time: {
+      departure: '10:20',
+    },
+  },
+  {
+    flight: 'DD9216',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUD',
+    },
+    aircraft: '738',
+    time: {
+      departure: '10:25',
+    },
+  },
+  {
+    flight: 'SL544',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCT',
+    },
+    aircraft: 'B738',
+    time: {
+      departure: '10:25',
+    },
+  },
+  {
+    flight: 'SL826',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTST',
+    },
+    aircraft: 'B738',
+    time: {
+      departure: '10:25',
+    },
+  },
+  {
+    flight: 'DD8522',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCL',
+    },
+    aircraft: 'DH4',
+    time: {
+      departure: '10:30',
+    },
+  },
+  {
+    flight: 'FD3110',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSS',
+    },
+    aircraft: '320',
+    time: {
+      departure: '10:30',
+    },
+  },
+  {
+    flight: 'DD9608',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUO',
+    },
+    aircraft: 'DH4',
+    time: {
+      departure: '10:35',
+    },
+  },
+  {
+    flight: 'FD891',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZGMX',
+    },
+    aircraft: '320',
+    time: {
+      departure: '10:35',
+    },
+  },
+  {
+    flight: 'DD7318',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSR',
+    },
+    aircraft: 'DH4',
+    time: {
+      departure: '10:40',
+    },
+  },
+  {
+    flight: 'FD502',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VHHH',
+    },
+    aircraft: 'A320',
+    time: {
+      departure: '10:40',
+    },
+  },
+  {
+    flight: 'FD3027',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSP',
+    },
+    aircraft: '320',
+    time: {
+      departure: '10:50',
+    },
+  },
+  {
+    flight: 'FD540',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZGHA',
+    },
+    aircraft: '320',
+    time: {
+      departure: '11:00',
+    },
+  },
+  {
+    flight: 'FD3372',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUU',
+    },
+    aircraft: '320',
+    time: {
+      departure: '11:00',
+    },
+  },
+  {
+    flight: 'FD257',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VYYY',
+    },
+    aircraft: '320',
+    time: {
+      departure: '11:05',
+    },
+  },
+  {
+    flight: 'FD3356',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUD',
+    },
+    aircraft: '320',
+    time: {
+      departure: '11:10',
+    },
+  },
+  {
+    flight: 'FD3429',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCC',
+    },
+    aircraft: '320',
+    time: {
+      departure: '11:10',
+    },
+  },
+  {
+    flight: 'OD523',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'WMKK',
+    },
+    aircraft: '738',
+    time: {
+      departure: '11:10',
+    },
+  },
+  {
+    flight: 'E3811',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZSCN',
+    },
+    aircraft: '737',
+    time: {
+      departure: '11:15',
+    },
+  },
+  {
+    flight: 'FD353',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'WSSS',
+    },
+    aircraft: '320',
+    time: {
+      departure: '11:15',
+    },
+  },
+  {
+    flight: 'FD766',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VMMC',
+    },
+    aircraft: '320',
+    time: {
+      departure: '11:15',
+    },
+  },
+  {
+    flight: 'SL546',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCT',
+    },
+    aircraft: 'B739',
+    time: {
+      departure: '11:15',
+    },
+  },
+  {
+    flight: 'DD9410',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUI',
+    },
+    aircraft: '738',
+    time: {
+      departure: '11:20',
+    },
+  },
+  {
+    flight: 'SL746',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSB',
+    },
+    aircraft: 'B738',
+    time: {
+      departure: '11:20',
+    },
+  },
+  {
+    flight: 'E3801',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZSHC',
+    },
+    aircraft: '737',
+    time: {
+      departure: '11:25',
+    },
+  },
+  {
+    flight: 'FD644',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VVNB',
+    },
+    aircraft: '320',
+    time: {
+      departure: '11:25',
+    },
+  },
+  {
+    flight: 'FD3182',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSF',
+    },
+    aircraft: '320',
+    time: {
+      departure: '11:25',
+    },
+  },
+  {
+    flight: 'FD9821',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCC',
+    },
+    aircraft: '320',
+    time: {
+      departure: '11:25',
+    },
+  },
+  {
+    flight: 'DD3218',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VVTS',
+    },
+    aircraft: '738',
+    time: {
+      departure: '11:30',
+    },
+  },
+  {
+    flight: 'FD596',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZGSZ',
+    },
+    aircraft: '320',
+    time: {
+      departure: '11:30',
+    },
+  },
+  {
+    flight: 'AK1808',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'WMKJ',
+    },
+    aircraft: 'A320',
+    time: {
+      departure: '11:35',
+    },
+  },
+  {
+    flight: 'FD3212',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSG',
+    },
+    aircraft: '320',
+    time: {
+      departure: '11:35',
+    },
+  },
+  {
+    flight: 'DD7520',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSP',
+    },
+    aircraft: '738',
+    time: {
+      departure: '11:40',
+    },
+  },
+  {
+    flight: 'DD8414',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTPP',
+    },
+    aircraft: 'DH4',
+    time: {
+      departure: '11:40',
+    },
+  },
+  {
+    flight: 'SL922',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZSNJ',
+    },
+    aircraft: 'B739',
+    time: {
+      departure: '11:50',
+    },
+  },
+  {
+    flight: 'SL206',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VYYY',
+    },
+    aircraft: 'B738',
+    time: {
+      departure: '11:55',
+    },
+  },
+  {
+    flight: 'SL940',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZHHH',
+    },
+    aircraft: 'B738',
+    time: {
+      departure: '11:55',
+    },
+  },
+  {
+    flight: 'DD8324',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCC',
+    },
+    aircraft: '738',
+    time: {
+      departure: '12:00',
+    },
+  },
+  {
+    flight: 'FD3009',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSP',
+    },
+    aircraft: '320',
+    time: {
+      departure: '12:00',
+    },
+  },
+  {
+    flight: 'SL920',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZSHC',
+    },
+    aircraft: 'B739',
+    time: {
+      departure: '12:00',
+    },
+  },
+  {
+    flight: 'FD530',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZGGG',
+    },
+    aircraft: '320',
+    time: {
+      departure: '12:05',
+    },
+  },
+  {
+    flight: 'SL118',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'WIII',
+    },
+    aircraft: 'B738',
+    time: {
+      departure: '12:05',
+    },
+  },
+  {
+    flight: 'FD3568',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUV',
+    },
+    aircraft: '320',
+    time: {
+      departure: '12:10',
+    },
+  },
+  {
+    flight: 'DD6444',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSS',
+    },
+    aircraft: '738',
+    time: {
+      departure: '12:15',
+    },
+  },
+  {
+    flight: 'FD3233',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSB',
+    },
+    aircraft: '320',
+    time: {
+      departure: '12:20',
+    },
+  },
+  {
+    flight: 'IT506',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'RCTP',
+    },
+    aircraft: '320',
+    time: {
+      departure: '12:20',
+    },
+  },
+  {
+    flight: 'FD658',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VVTS',
+    },
+    aircraft: '320',
+    time: {
+      departure: '12:25',
+    },
+  },
+  {
+    flight: 'FD3443',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCC',
+    },
+    aircraft: '320',
+    time: {
+      departure: '12:25',
+    },
+  },
+  {
+    flight: 'SL394',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'RCTP',
+    },
+    aircraft: 'B738',
+    time: {
+      departure: '12:25',
+    },
+  },
+  {
+    flight: 'DD4238',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VYYY',
+    },
+    aircraft: '738',
+    time: {
+      departure: '12:30',
+    },
+  },
+  {
+    flight: 'XW858',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZSPD',
+    },
+    aircraft: '772',
+    time: {
+      departure: '12:30',
+    },
+  },
+  {
+    flight: 'FD398',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'WADD',
+    },
+    aircraft: '320',
+    time: {
+      departure: '12:35',
+    },
+  },
+  {
+    flight: 'FD3011',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSP',
+    },
+    aircraft: '320',
+    time: {
+      departure: '12:35',
+    },
+  },
+  {
+    flight: 'FD3258',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUK',
+    },
+    aircraft: '320',
+    time: {
+      departure: '12:35',
+    },
+  },
+  {
+    flight: 'SL616',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUD',
+    },
+    aircraft: 'B739',
+    time: {
+      departure: '12:35',
+    },
+  },
+  {
+    flight: 'SL634',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUU',
+    },
+    aircraft: 'B738',
+    time: {
+      departure: '12:35',
+    },
+  },
+  {
+    flight: 'DD9320',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUU',
+    },
+    aircraft: '738',
+    time: {
+      departure: '12:45',
+    },
+  },
+  {
+    flight: 'DD9820',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUK',
+    },
+    aircraft: 'DH4',
+    time: {
+      departure: '12:45',
+    },
+  },
+  {
+    flight: 'DD7116',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSS',
+    },
+    aircraft: '738',
+    time: {
+      departure: '12:55',
+    },
+  },
+  {
+    flight: 'FD618',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VDSR',
+    },
+    aircraft: '320',
+    time: {
+      departure: '13:00',
+    },
+  },
+  {
+    flight: 'FD3112',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSS',
+    },
+    aircraft: '320',
+    time: {
+      departure: '13:05',
+    },
+  },
+  {
+    flight: 'FD3205',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCT',
+    },
+    aircraft: '320',
+    time: {
+      departure: '13:05',
+    },
+  },
+  {
+    flight: 'DD9218',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUD',
+    },
+    aircraft: '738',
+    time: {
+      departure: '13:10',
+    },
+  },
+  {
+    flight: 'DD8326',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCC',
+    },
+    aircraft: '738',
+    time: {
+      departure: '13:15',
+    },
+  },
+  {
+    flight: 'AK889',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'WMKK',
+    },
+    aircraft: 'A320',
+    time: {
+      departure: '13:20',
+    },
+  },
+  {
+    flight: 'FD3033',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSP',
+    },
+    aircraft: '320',
+    time: {
+      departure: '13:20',
+    },
+  },
+  {
+    flight: 'FD9817',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCC',
+    },
+    aircraft: '320',
+    time: {
+      departure: '13:25',
+    },
+  },
+  {
+    flight: 'FD604',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VDPP',
+    },
+    aircraft: '320',
+    time: {
+      departure: '13:30',
+    },
+  },
+  {
+    flight: 'FD3350',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTUD',
+    },
+    aircraft: '320',
+    time: {
+      departure: '13:30',
+    },
+  },
+  {
+    flight: 'SL724',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSS',
+    },
+    aircraft: 'B739',
+    time: {
+      departure: '13:30',
+    },
+  },
+  {
+    flight: 'DD7524',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSP',
+    },
+    aircraft: '738',
+    time: {
+      departure: '13:35',
+    },
+  },
+  {
+    flight: 'FD3013',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSP',
+    },
+    aircraft: '320',
+    time: {
+      departure: '13:35',
+    },
+  },
+  {
+    flight: 'QZ253',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'WIII',
+    },
+    aircraft: '320',
+    time: {
+      departure: '13:40',
+    },
+  },
+  {
+    flight: 'SL518',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCC',
+    },
+    aircraft: 'A333',
+    time: {
+      departure: '13:45',
+    },
+  },
+  {
+    flight: 'FD137',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VOBL',
+    },
+    aircraft: '320',
+    time: {
+      departure: '13:55',
+    },
+  },
+  {
+    flight: 'SL522',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCC',
+    },
+    aircraft: 'B739',
+    time: {
+      departure: '14:10',
+    },
+  },
+  {
+    flight: 'FD153',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VOMM',
+    },
+    aircraft: '320',
+    time: {
+      departure: '14:15',
+    },
+  },
+  {
+    flight: 'FD3124',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSS',
+    },
+    aircraft: '320',
+    time: {
+      departure: '14:15',
+    },
+  },
+  {
+    flight: 'FD3427',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCC',
+    },
+    aircraft: '320',
+    time: {
+      departure: '14:15',
+    },
+  },
+  {
+    flight: 'Z2288',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'RPLL',
+    },
+    aircraft: '320',
+    time: {
+      departure: '14:20',
+    },
+  },
+  {
+    flight: 'FD3015',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSP',
+    },
+    aircraft: '320',
+    time: {
+      departure: '14:25',
+    },
+  },
+  {
+    flight: 'AK897',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'WMKK',
+    },
+    aircraft: 'A320',
+    time: {
+      departure: '14:30',
+    },
+  },
+  {
+    flight: 'SL748',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSB',
+    },
+    aircraft: 'B738',
+    time: {
+      departure: '14:35',
+    },
+  },
+  {
+    flight: 'FD584',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZPPP',
+    },
+    aircraft: '320',
+    time: {
+      departure: '14:40',
+    },
+  },
+  {
+    flight: 'SL900',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZGGG',
+    },
+    aircraft: 'B738',
+    time: {
+      departure: '14:40',
+    },
+  },
+  {
+    flight: 'FD562',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZUUU',
+    },
+    aircraft: '320',
+    time: {
+      departure: '14:50',
+    },
+  },
+  {
+    flight: 'FD3419',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTCC',
+    },
+    aircraft: '320',
+    time: {
+      departure: '14:50',
+    },
+  },
+  {
+    flight: 'FD3023',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSP',
+    },
+    aircraft: '320',
+    time: {
+      departure: '14:55',
+    },
+  },
+  {
+    flight: 'SL726',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSS',
+    },
+    aircraft: 'B738',
+    time: {
+      departure: '15:05',
+    },
+  },
+  {
+    flight: 'FD3215',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VTSG',
+    },
+    aircraft: '320',
+    time: {
+      departure: '15:10',
+    },
+  },
+  {
+    flight: 'SL932',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZUUU',
+    },
+    aircraft: 'B739',
+    time: {
+      departure: '15:25',
+    },
+  },
+  {
+    flight: 'FD112',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VEBS',
+    },
+    aircraft: '320',
+    time: {
+      departure: '15:35',
+    },
+  },
+  {
+    flight: 'FD170',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VOCI',
+    },
+    aircraft: '320',
+    time: {
+      departure: '15:35',
+    },
+  },
+  {
+    flight: 'XW306',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VIDP',
+    },
+    aircraft: '772',
+    time: {
+      departure: '15:40',
+    },
+  },
+  {
+    flight: 'DD4112',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VEVZ',
+    },
+    aircraft: '738',
+    time: {
+      departure: '16:00',
+    },
+  },
+  {
+    flight: 'SL902',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'ZGSZ',
+    },
+    aircraft: 'B739',
+    time: {
+      departure: '16:05',
+    },
+  },
+  {
+    flight: 'SL314',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'RJFF',
+    },
+    aircraft: 'B738',
+    time: {
+      departure: '16:20',
+    },
+  },
+  {
+    flight: 'XW112',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'RJBB',
+    },
+    aircraft: '772',
+    time: {
+      departure: '16:40',
+    },
+  },
+  {
+    flight: 'SL224',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'VGHS',
+    },
+    aircraft: 'B739',
+    time: {
+      departure: '16:45',
+    },
+  },
+  {
+    flight: 'XJ600',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'RJAA',
+    },
+    aircraft: '333',
+    time: {
+      departure: '16:55',
+    },
+  },
+  {
+    flight: 'XJ620',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'RJCC',
+    },
+    aircraft: '333',
+    time: {
+      departure: '16:55',
+    },
+  },
+  {
+    flight: 'XJ638',
+    airport: {
+      departure: 'VTBD',
+      arrival: 'RJGG',
+    },
+    aircraft: '333',
+    time: {
+      departure: '16:55',
+    },
+  },
+]
