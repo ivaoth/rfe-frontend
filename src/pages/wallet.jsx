1f1b72dc067e9ac963f329932c8a42c88cbe1707
Phumrapee Limpianchop <git@rayriffy.com>
2019-05-01 17:32:44 +0700

    :sparkles: Show more info on Wallet

--- src/pages/wallet.jsx
+++ src/pages/wallet.jsx
@@ -4,15 +4,18 @@ import PropTypes from 'prop-types'
 
 import {Axios, appContext, Loading} from '../bridge'
 
-import {Row, Col, Typography, List, Modal, message} from 'antd'
+import {Row, Col, Typography, List, Modal, message, Card, Icon, Divider} from 'antd'
 
 const {Title, Text} = Typography
+const {Meta} = Card
 
 const Wallet = props => {
   const [raw, setRaw] = useState(null)
   const [error, setError] = useState(false)
   const [isLoading, setIsLoading] = useState(true)
   const [asyncInProgress, setAsyncInProgress] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+  const [modalRaw, setModalRaw] = useState(null)
 
   const {store} = props
 
@@ -74,6 +77,17 @@ const Wallet = props => {
     })
   }
 
+  const modalInfo = async item => {
+    setModalRaw(null)
+    setShowModal(true)
+
+    const route = await Axios.get(
+      `${store.apiEndpoint}/api/v1/route/get/${item.flight.airport.departure}/${item.flight.airport.arrival}`,
+    )
+
+    setModalRaw({...item, route: route.data.response.data.route})
+  }
+
   return (
     <Row>
       <Col xs={{span: 22, offset: 1}} sm={{span: 18, offset: 3}} md={{span: 14, offset: 5}} lg={{span: 10, offset: 7}}>
@@ -100,12 +114,18 @@ const Wallet = props => {
                             onClick={() => modalCancel(item)}>
                             cancel
                           </Text>,
-                          <Text key={`${item.event.id}-${item.flight.id}-button-more`} disabled>
+                          <Text
+                            style={{color: '#1890ff'}}
+                            key={`${item.event.id}-${item.flight.id}-button-more`}
+                            onClick={() => modalInfo(item)}>
                             more
                           </Text>,
                         ]
                       : [
-                          <Text key={`${item.event.id}-${item.flight.id}-button-more`} disabled>
+                          <Text
+                            style={{color: '#1890ff'}}
+                            key={`${item.event.id}-${item.flight.id}-button-more`}
+                            onClick={() => modalInfo(item)}>
                             more
                           </Text>,
                         ]
@@ -114,6 +134,84 @@ const Wallet = props => {
                 </List.Item>
               )}
             />
+
+            <Modal
+              title={modalRaw === null ? `Loading` : `Flight information for ${modalRaw.flight.flight}`}
+              visible={showModal}
+              width={700}
+              onOk={() => setShowModal(false)}>
+              {modalRaw === null ? (
+                <Loading />
+              ) : (
+                <>
+                  <Row gutter={16}>
+                    <Col xs={{span: 24}} md={{span: 10}}>
+                      <Row>
+                        <Title level={3}>Event</Title>
+                      </Row>
+                      <Row style={{margin: '10px 0'}}>
+                        <Card cover={<img alt={modalRaw.event.name} src={modalRaw.event.cover} />}>
+                          <Meta title={modalRaw.event.name} description={modalRaw.event.desc} />
+                        </Card>
+                      </Row>
+                    </Col>
+                    <Col xs={{span: 24}} md={{span: 14}}>
+                      <Row>
+                        <Title level={3}>Flight</Title>
+                      </Row>
+                      <Row style={{margin: '10px 0'}}>
+                        <Card span={24}>
+                          <Title level={4}>{modalRaw.flight.flight}</Title>
+                          <Row>
+                            <Col span={24}>
+                              {modalRaw.flight.airport.departure}{' '}
+                              {modalRaw.flight.bay !== null && modalRaw.flight.bay.departure !== null
+                                ? `(${modalRaw.flight.bay.departure})`
+                                : null}{' '}
+                              <Icon type="right" /> {modalRaw.flight.airport.arrival}{' '}
+                              {modalRaw.flight.bay !== null && modalRaw.flight.bay.arrival !== null
+                                ? `(${modalRaw.flight.bay.arrival})`
+                                : null}
+                            </Col>
+                          </Row>
+                          <Divider />
+                          <Row>
+                            <Col span={24}>
+                              <Text strong>Airline ICAO</Text> {modalRaw.flight.airline.code}
+                            </Col>
+                          </Row>
+                          <Row>
+                            <Col span={24}>
+                              <Text strong>Aircraft</Text> {modalRaw.flight.type}
+                            </Col>
+                          </Row>
+                          <Row>
+                            <Col span={24}>
+                              <Text strong>Departure time</Text> {modalRaw.flight.time.departure}
+                            </Col>
+                          </Row>
+                          <Row>
+                            <Col span={24}>
+                              <Text strong>EST. arrival time</Text> {modalRaw.flight.time.arrival}
+                            </Col>
+                          </Row>
+                          <Row>
+                            <Col span={24}>
+                              <Text strong>Distance</Text> {modalRaw.flight.distance} nm
+                            </Col>
+                          </Row>
+                          <Row>
+                            <Col span={24}>
+                              <Text strong>Route</Text> {modalRaw.route.route}
+                            </Col>
+                          </Row>
+                        </Card>
+                      </Row>
+                    </Col>
+                  </Row>
+                </>
+              )}
+            </Modal>
           </Row>
         )}
       </Col>
